f160c357be5210fa9b1f386b72b122da71911a39
Ismail <ismailmunyentwari9@gmail.com>
2023-01-24 17:06:41 +0200

    y webpack project

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import _ from 'lodash';
+import './style.css';
+
+ function component() {
+   
+
+  // Lodash, currently included via a script, is required for this line to work
+  // Lodash, now imported by this script
+  
+ }
+
+ 
